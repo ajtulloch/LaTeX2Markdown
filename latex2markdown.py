1fbbfc177fb6cf6df6946185605bc289ad0df642
Andrew Tulloch <ajtulloch@gmail.com>
2012-03-31 16:16:57 +0800

    Refactored into a class

--- latex2markdown.py
+++ latex2markdown.py
@@ -273,9 +273,8 @@ header_re = re.compile(r"""\\(?P<header_name>chapter|section|subsection)
                             {(?P<header_contents>.*?)}""", #
                             flags=re.DOTALL + re.VERBOSE)
 
-block_counter = defaultdict(lambda: 1)
 
-block_configuration = {
+_block_configuration = {
     'exer': {
         "markdown_heading": "####",
         "pretty_name": "Exercise",
@@ -315,86 +314,112 @@ block_configuration = {
     }
 }
 
-def replace_header(matchobj):
-    header_name = matchobj.group('header_name')
-    header_contents = matchobj.group('header_contents')
-    
-    header = format_block_name(header_name)
-    return "{header} - {title}\n".format(header=header, title=header_contents)
+class LaTeX2Markdown(object):
+    def __init__(self, latex_string, block_configuration = _block_configuration, block_counter = defaultdict(lambda: 1)):
+        self._block_configuration = block_configuration
+        self._latex_string = latex_string
+        self._block_counter = block_counter
+        self._main_re = re.compile(r"""\\begin{document}
+                                    (?P<main>.*)
+                                    \\end{document}""", 
+                                    flags=re.DOTALL + re.VERBOSE)
 
-def replace_block(matchobj):
-    block_name = matchobj.group('block_name')
-    block_contents = matchobj.group('block_contents')
+        self._block_re = re.compile(r"""\\begin{(?P<block_name>exer|proof|thm|lem|prop)} # block name
+                                    (?P<block_contents>.*?) # Non-greedy block contents
+                                    \\end{(?P=block_name)}""", # closing block
+                                    flags=re.DOTALL + re.VERBOSE)
+
+        self._lists_re = re.compile(r"""\\begin{(?P<block_name>enumerate|itemize)} # list name
+                                    \[\(.\)\]? # Optional enumerate settings i.e. (a)
+                                    (?P<block_contents>.*?) # Non-greedy list contents
+                                    \\end{(?P=block_name)}""", # closing list
+                                    flags=re.DOTALL + re.VERBOSE)
+
+        self._header_re = re.compile(r"""\\(?P<header_name>chapter|section|subsection)
+                                    {(?P<header_contents>.*?)}""", #
+                                    flags=re.DOTALL + re.VERBOSE)
+
+    def _replace_header(self, matchobj):
+        header_name = matchobj.group('header_name')
+        header_contents = matchobj.group('header_contents')
     
-    if block_name in {"itemize", "enumerate"}:
-        formatted_contents = format_list_contents(block_name, 
-                                                    block_contents)
-    else:
-        formatted_contents = format_block_contents(block_name,
-                                                    block_contents)
+        header = self._format_block_name(header_name)
+        return "{header} - {title}\n".format(header=header, title=header_contents)
+
+    def _replace_block(self, matchobj):
+        block_name = matchobj.group('block_name')
+        block_contents = matchobj.group('block_contents')
     
-    header = format_block_name(block_name)
+        if block_name in {"itemize", "enumerate"}:
+            formatted_contents = self._format_list_contents(block_name, 
+                                                        block_contents)
+        else:
+            formatted_contents = self._format_block_contents(block_name,
+                                                        block_contents)
     
-    output_str = "{header}\n\n{block_contents}".format(
-                    header=header, 
-                    block_contents=formatted_contents)    
-    return output_str
+        header = self._format_block_name(block_name)
+    
+        output_str = "{header}\n\n{block_contents}".format(
+                        header=header, 
+                        block_contents=formatted_contents)    
+        return output_str
 
 
-def format_block_contents(block_name, block_contents):
-    line_indent_char = block_configuration[block_name]["line_indent_char"]
+    def _format_block_contents(self, block_name, block_contents):
+        line_indent_char = self._block_configuration[block_name]["line_indent_char"]
     
-    output_str = ""
-    for line in block_contents.lstrip().rstrip().split("\n"):
-        line = line.lstrip().rstrip() 
-        indented_line = line_indent_char + line + "\n"
-        output_str += indented_line   
-    return output_str
+        output_str = ""
+        for line in block_contents.lstrip().rstrip().split("\n"):
+            line = line.lstrip().rstrip() 
+            indented_line = line_indent_char + line + "\n"
+            output_str += indented_line   
+        return output_str
 
-def format_list_contents(block_name, block_contents):
-    list_heading = block_configuration[block_name]["list_heading"]
+    def _format_list_contents(self, block_name, block_contents):
+        list_heading = self._block_configuration[block_name]["list_heading"]
     
-    output_str = ""
-    for line in block_contents.lstrip().rstrip().split("\n"):
-        line = line.lstrip().rstrip() 
-        markdown_list_line = line.replace(r"\item", list_heading)
-        output_str += markdown_list_line + "\n"
-    return output_str
+        output_str = ""
+        for line in block_contents.lstrip().rstrip().split("\n"):
+            line = line.lstrip().rstrip() 
+            markdown_list_line = line.replace(r"\item", list_heading)
+            output_str += markdown_list_line + "\n"
+        return output_str
     
-def format_block_name(block_name):
-    pretty_name = block_configuration[block_name]["pretty_name"]
-    show_count = block_configuration[block_name]["show_count"]
-    markdown_heading = block_configuration[block_name]["markdown_heading"]
+    def _format_block_name(self, block_name):
+        block_config = self._block_configuration[block_name]
+        pretty_name = block_config["pretty_name"]
+        show_count = block_config["show_count"]
+        markdown_heading = block_config["markdown_heading"]
     
-    block_count = block_counter[block_name] if show_count else ""
+        block_count = self._block_counter[block_name] if show_count else ""
+        self._block_counter[block_name] += 1
     
-    block_counter[block_name] += 1
+        output_str = "{markdown_heading} {pretty_name} {block_count}".format(
+                        markdown_heading=markdown_heading, 
+                        pretty_name=pretty_name, 
+                        block_count=block_count)
     
-    output_str = "{markdown_heading} {pretty_name} {block_count}".format(
-                    markdown_heading=markdown_heading, 
-                    pretty_name=pretty_name, 
-                    block_count=block_count)
+        return output_str.lstrip().rstrip()
+    def latex_to_markdown(self):
+        # Get main content, skipping preamble and closing tags.
+        output = self._main_re.search(self._latex_string).group("main")
     
-    return output_str.lstrip().rstrip()
-def latex_to_markdown(latex_string):
-    # Get main content, skipping preamble and closing tags.
-    output = main_re.search(latex_string).group("main")
+        # Reformat, lists, blocks, and headers.
+        output = self._lists_re.sub(self._replace_block, output)
+        output = self._block_re.sub(self._replace_block, output)
+        output = self._header_re.sub(self._replace_header, output)
     
-    # Reformat, lists, blocks, and headers.
-    output = lists_re.sub(replace_block, output)
-    output = block_re.sub(replace_block, output)
-    output = header_re.sub(replace_header, output)
-    
-    # Fix \\ formatting for line breaks in align blocks  
-    output = re.sub(r" \\\\", r" \\\\\\\\", output)
-    # Fix Align* block formatting
-    output = re.sub(r"align\*", r"align", output)
-    # Fix emph{} formatting
-    output = re.sub(r"\\emph{(.*?)}", r"*\1*", output)
-    # Fix \% formatting
-    output = re.sub(r"\\%", r"%", output)
-    # Fix argmax, etc.
-    output = re.sub(r"\\arg(max|min)", r"\\text{arg\1}", output)
-    return output
+        # Fix \\ formatting for line breaks in align blocks  
+        output = re.sub(r" \\\\", r" \\\\\\\\", output)
+        # Fix Align* block formatting
+        output = re.sub(r"align\*", r"align", output)
+        # Fix emph{} formatting
+        output = re.sub(r"\\emph{(.*?)}", r"*\1*", output)
+        # Fix \% formatting
+        output = re.sub(r"\\%", r"%", output)
+        # Fix argmax, etc.
+        output = re.sub(r"\\arg(max|min)", r"\\text{arg\1}", output)
+        return output
 
-print latex_to_markdown(test)
+y = LaTeX2Markdown(test)
+print y.latex_to_markdown()
